67b3e7dfc7924582b7ff60dae2b810f39c620fbd
David Driscoll <david.driscoll@gmail.com>
2020-06-17 20:36:40 -0400

    Drop nukeeper (#28)

--- consolidate-nukeeper/src/main.ts
+++
@@ -1,9 +0,0 @@
-// import { setFailed, getInput } from '@actions/core';
-// import { getOctokit, context } from '@actions/github';
-// import { exec } from '@actions/exec';
-
-// async function run(): Promise<void> {
-//     context.workflow;
-// }
-
-// run();

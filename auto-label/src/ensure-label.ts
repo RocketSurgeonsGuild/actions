691cf7879d0c3e84da216423e2d06fc47ff05fef
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2021-02-09 23:30:50 -0600

    Added auto label action

---
+++ auto-label/src/ensure-label.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { map, filter } from 'rxjs/operators';
+import { getOctokit } from '@actions/github';
+
+type GitHub = ReturnType<typeof getOctokit>;
+
+
+export async function addPullRequestLabel(
+    github: GitHub,
+    request: { owner: string; repo: string },
+    pr: import('@octokit/types/dist-types/generated/Endpoints').PullsGetResponseData
+) {
+    const title = pr.title;
+    const titleLabel = pr.labels.filter(z => z.name.includes(title));
+    const hasLabel = titleLabel.length > 0;
+
+    console.log(`label ${hasLabel ? 'found' : 'not found'}`, pr.labels);
+    if (hasLabel) return;
+
+    console.log('adding default label', titleLabel);
+    await github.issues.addLabels({
+        ...request,
+        issue_number: pr.number,
+        labels: [titleLabel],
+    });
+}
